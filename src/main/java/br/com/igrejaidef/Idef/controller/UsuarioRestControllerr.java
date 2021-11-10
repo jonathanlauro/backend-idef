afc301e72ea4770b93e7706c2db94930c7fd6538
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-11-10 15:00:59 -0300

    troca de endpoint

--- src/main/java/br/com/igrejaidef/Idef/controller/UsuarioRestControllerr.java
+++ src/main/java/br/com/igrejaidef/Idef/controller/UsuarioRestControllerr.java
@@ -4,6 +4,7 @@ import br.com.igrejaidef.Idef.dto.modelo.Usuario;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -23,6 +24,6 @@ public interface UsuarioRestControllerr {
     ResponseEntity<Usuario> deletarUsuario(@RequestParam long id) throws Exception;
 
     @GetMapping("/{login}/buscarole")
-    ResponseEntity<String> buscaRole(@RequestParam(name = "login") String login) throws Exception;
+    ResponseEntity<String> buscaRole(@PathParam("login") String login) throws Exception;
 
 }
--- src/main/java/br/com/igrejaidef/Idef/controller/impl/UsuarioRestControllerImpl.java
+++ src/main/java/br/com/igrejaidef/Idef/controller/impl/UsuarioRestControllerImpl.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -40,7 +41,7 @@ public class UsuarioRestControllerImpl implements UsuarioRestControllerr {
     }
 
     @Override
-    public ResponseEntity<String> buscaRole(@RequestParam String login) throws Exception {
+    public ResponseEntity<String> buscaRole(String login) throws Exception {
         return ResponseEntity.ok(repository.buscaRole(login));
     }
 
